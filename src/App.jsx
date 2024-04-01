b86541a8ce99f4ff31689ed89397ff0192956405
alex <sytnikov.zp@gmail.com>
2024-04-01 03:13:33 +0300

    Test commit in MacOS

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ import Header from './components/Header/Header';
 import './App.css';
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     count: 0,
-  //   };
-  // }
+
 
   state = {
     count: 0,
